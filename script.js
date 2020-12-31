bdfe7526fadbba7dcac234ac2a642d160fd4519d
Econmartin <73027064+Econmartin@users.noreply.github.com>
2020-12-31 00:58:11 +0000

    first attempt at completion

--- script.js
+++ script.js
@@ -2,5 +2,41 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
-
+// Generate the secretTarget to try and guess
+function generateTarget() {
+    return Math.floor(Math.random() * 10)
+}
+  
+// Find absolute distance using just two inputs
+const getAbsoluteDistance = (a, b) => Math.abs(a - b);
+  
+// Compare distances between human and computer. human wins if tie.
+const compareGuesses = (humanGuess, computerGuess, secretTarget) => {
+    // Add window alert for outside range
+    if ((humanGuess < 0) || (humanGuess > 9)){
+     window.alert('Outside Range');
+    }
+     // Human wins if closer or tie
+    else if ((getAbsoluteDistance(humanGuess, secretTarget)) <= (getAbsoluteDistance(computerGuess, secretTarget))){
+        return true;
+    }
+    // computer wins
+    else {
+        return false;
+    };
+};
+  
+// Update score for winner
+function updateScore(winner){
+    if (winner === 'human') {
+      humanScore += 1;
+    } else {
+      computerScore += 1;
+    };
+  };
+  
+// Move to next round
+function advanceRound() {
+    currentRoundNumber += 1;
+};
+  
